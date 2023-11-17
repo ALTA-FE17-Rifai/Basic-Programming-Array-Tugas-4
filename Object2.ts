01cddc8dce1a27eb5450ddd1c6125d9708ca430a
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-17 17:46:26 +0700

    Object2.ts

--- Object2.ts
+++ Object2.ts
@@ -1,6 +1,10 @@
 function meanMedian(arrayInput) {
 
-
+let arr = ["1", "2", "3", 
+    "4", "5", "6","7", "8", "9", "13", "15", "18", "20", "30", "40", "50", "60", "120" ]; 
+    function removeDuplicates(arr) { 
+      return [...new Set(arr)];
+        
     console.log(meanMedian([1, 2, 3, 4]));
     console.log(meanMedian([1, 2, 3, 4, 5]));
     console.log(meanMedian([7, 8, 9, 13, 15]));

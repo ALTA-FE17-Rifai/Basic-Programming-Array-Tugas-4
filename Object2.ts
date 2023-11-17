65dd2da4065504c592fa8291d5a61033e73c8a78
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-17 17:35:55 +0700

    Tugas 3 Mean & Median

---
+++ Object2.ts
@@ -0,0 +1,8 @@
+function meanMedian(arrayInput) {
+
+
+    console.log(meanMedian([1, 2, 3, 4]));
+    console.log(meanMedian([1, 2, 3, 4, 5]));
+    console.log(meanMedian([7, 8, 9, 13, 15]));
+    console.log(meanMedian([18, 20, 30, 40, 50]));
+    console.log(meanMedian([15, 20, 30, 60, 120]));
